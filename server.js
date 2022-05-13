aa31546badf274d4266d1833ccc20fde53b20a1b
Adam Brock <a.paulbrock@gmail.com>
2022-05-13 15:02:20 -0400

    Changed PORT variable to account for deployment

--- server.js
+++ server.js
@@ -2,19 +2,35 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 const uuid = require("./helpers/uuid");
-const PORT = 3001; //need to convert make this switch based on who's accessing it
-
+const PORT = process.env.PORT || 3001;
+const notes = require("./db/db.json");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/index.html"))
-);
+app.use(express.static("public"));
 
-// TODO: GET request for notes page
+// GET request for the landing page
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+  console.log("Successful GET request to /");
+});
+
+// GET request for notes page
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+  console.log("Successful GET request to /notes");
+});
 
 // TODO: GET request for notes
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+
+  console.log("Successful GET request to /api/notes");
+});
 
 // TODO: POST request for notes
 
@@ -22,7 +38,70 @@ app.get("/", (req, res) =>
 
 // // TODO: DELETE request for notes
 
+// ------
+// // GET request for the notes
+// app.get("/api/reviews", (req, res) => {
+//   // Send a message to the client
+//   res.status(200).json(`${req.method} request received to get reviews`);
 
+//   // Log our request to the terminal
+//   console.info(`${req.method} request received to get reviews`);
+// });
+
+// ------
+// POST request to add a review
+
+// app.post('/api/reviews', (req, res) => {
+//   // Log that a POST request was received
+//   console.info(`${req.method} request received to add a review`);
+
+//   // Destructuring assignment for the items in req.body
+//   const { product, review, username } = req.body;
+
+//   // If all the required properties are present
+//   if (product && review && username) {
+//     // Variable for the object we will save
+//     const newReview = {
+//       product,
+//       review,
+//       username,
+//       review_id: uuid(),
+//     };
+
+//     // Obtain existing reviews
+//     fs.readFile('./db/reviews.json', 'utf8', (err, data) => {
+//       if (err) {
+//         console.error(err);
+//       } else {
+//         // Convert string into JSON object
+//         const parsedReviews = JSON.parse(data);
+
+//         // Add a new review
+//         parsedReviews.push(newReview);
+
+//         // Write updated reviews back to the file
+//         fs.writeFile(
+//           './db/reviews.json',
+//           JSON.stringify(parsedReviews, null, 4),
+//           (writeErr) =>
+//             writeErr
+//               ? console.error(writeErr)
+//               : console.info('Successfully updated reviews!')
+//         );
+//       }
+//     });
+
+//     const response = {
+//       status: 'success',
+//       body: newReview,
+//     };
+
+//     console.log(response);
+//     res.status(201).json(response);
+//   } else {
+//     res.status(500).json('Error in posting review');
+//   }
+// });
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
